6c9142262a2d19c31f87a003bdaf3e5ad3492c5f
Diego Mais <diegomais.dev@gmail.com>
2021-07-14 21:13:31 -0300

    🎨 add EditorConfig, ESLint and Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
-const port = 3333
+const app = express();
+const port = 3333;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
